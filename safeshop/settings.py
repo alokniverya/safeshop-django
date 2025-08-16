efee61d001e73eb7b94004203050a746c9e5d711
Alok Singh <alok@example.comcls>
2025-08-16 12:06:57 +0530

    django authentication

--- safeshop/settings.py
+++ safeshop/settings.py
@@ -136,3 +136,17 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger',
+}
+
+#SMTP Configration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'alokniverya1907@gmail.com'
+EMAIL_HOST_PASSWORD = 'gqixfrdxqvuxpgyw'
