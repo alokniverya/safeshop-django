f90aae18f73ba24a42eaba469a0704a9a16643d5
Alok Singh <alok@example.comcls>
2025-08-30 00:58:01 +0530

    fix: correct ALLOWED_HOSTS domain

--- safeshop/settings.py
+++ safeshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ["safeshop.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["safeshop-django.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
