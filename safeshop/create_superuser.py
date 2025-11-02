b84b8e84e018d2dca0237ecf9394602064238d9e
Alok Singh <alok@example.comcls>
2025-11-03 00:46:29 +0530

    update

--- safeshop/create_superuser.py
+++ safeshop/create_superuser.py
@@ -1,20 +1,13 @@
-import os
-import django
 from django.contrib.auth import get_user_model
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "safeshop.settings")
-django.setup()
 
 User = get_user_model()
 
-if not User.objects.filter(email="alokniverya1907@gmail.com").exists():
+if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser(
-        email="alokniverya1907@gmail.com",
         username="admin",
-        first_name="Alok",
-        last_name="Singh",
-        password="@devil99.com"
+        email="alokniverya1907@gmail.com",
+        password="@devil99.com"  
     )
-    print("✅ Superuser created successfully.")
+    print("Superuser created")
 else:
-    print("ℹ️ Superuser already exists.")
+    print("Superuser already exists")
