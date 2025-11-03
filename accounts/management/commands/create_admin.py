7264b8022b1ae8c35ce6be7a98b27b4688482066
Alok Singh <alok@example.comcls>
2025-11-03 12:18:00 +0530

    Fix superuser auto creation

---
+++ accounts/management/commands/create_admin.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create default superuser if it doesn't exist"
+
+    def handle(self, *args, **options):
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+
+        if not User.objects.filter(email=email).exists():
+            User.objects.create_superuser(
+                first_name="Admin",
+                last_name="User",
+                username="admin",
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS(f"✅ Superuser created: {email}"))
+        else:
+            self.stdout.write(self.style.WARNING("⚠️ Superuser already exists"))
