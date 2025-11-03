67e5698eb62765a5873611900a36150fc7dddfc9
Alok Singh <alok@example.comcls>
2025-11-03 11:46:00 +0530

    Fix superuser auto creation

--- safeshop/create_superuser.py
+++ safeshop/create_superuser.py
@@ -2,12 +2,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="alokniverya1907@gmail.com",
-        password="@devil99.com"  
-    )
-    print("Superuser created")
-else:
-    print("Superuser already exists")
+try:
+    if not User.objects.filter(email="alokniverya1907@gmail.com").exists():
+        User.objects.create_superuser(
+            first_name="Alok",
+            last_name="Singh",
+            username="admin",
+            email="alokniverya1907@gmail.com",
+            password="devil99.com"
+        )
+        print("✅ Superuser created successfully!")
+    else:
+        print("⚠️ Superuser already exists.")
+except Exception as e:
+    print("❌ Superuser creation failed:", e)
